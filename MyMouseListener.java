0ce881ff819a894a06874d097495de0ca54b58f3
Lim Mintaek <lmtna99@gmail.com>
2019-11-04 13:05:16 +0900

    change class name

--- MyMouseListener.java
+++ MyMouseListener.java
@@ -1,19 +1,19 @@
 
 /**
- * 여기에 Frame 클래스 설명을 작성하십시오.
+ * 여기에 MyMouseListener 클래스 설명을 작성하십시오.
  * 
  * @author (작성자 이름) 
  * @version (버전번호나 날짜)
  */
-public class Frame
+public class MyMouseListener
 {
     // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 바꾸십시오
     private int x;
 
     /**
-     * Frame 클래스의 객체 생성자
+     * MyMouseListener 클래스의 객체 생성자
      */
-    public Frame()
+    public MyMouseListener()
     {
         // 인스턴스 변수의 초기화
         x = 0;
